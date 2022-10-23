d4a1e9733acd247afd946957a61f8ed403deb4fb
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2022-10-23 12:37:34 +0700

    Update gateway and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocketioGateway } from './gateway/socketio.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SocketioGateway],
 })
 export class AppModule {}
