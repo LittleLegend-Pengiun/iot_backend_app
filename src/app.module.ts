17cd83756414bb8fd9cee262d35bf389e1d066d3
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2022-10-23 15:23:14 +0700

    Add controller and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MainController } from './controller/main/main.controller';
 import { SocketioGateway } from './gateway/socketio.gateway';
+import { MqttService } from './services/mqtt/mqtt/mqtt.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, SocketioGateway],
+  controllers: [MainController],
+  providers: [MqttService, SocketioGateway],
 })
 export class AppModule {}
