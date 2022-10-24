43171e1e3ade51df8603c116f941f69e8726e81f
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2022-10-24 10:52:53 +0700

    add new packages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AdminsController } from './admins/admins.controller';
+import { SocketIoGateway } from './gateway/socket-io.gateway';
+import { MqttService } from './mqtt/mqtt.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController, AdminsController],
-  providers: [AppService],
+  providers: [SocketIoGateway, MqttService],
 })
 export class AppModule {}
