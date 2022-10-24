38f4e96f9b424930188e0e3a09189ec11070364d
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2022-10-24 11:02:41 +0700

    update dockerignore

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-
+import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AdminsController } from './admins/admins.controller';
 import { SocketIoGateway } from './gateway/socket-io.gateway';
@@ -9,6 +9,6 @@ import { MqttService } from './mqtt/mqtt.service';
 @Module({
   imports: [UsersModule],
   controllers: [AppController, AdminsController],
-  providers: [SocketIoGateway, MqttService],
+  providers: [SocketIoGateway, MqttService, AppService],
 })
 export class AppModule {}
