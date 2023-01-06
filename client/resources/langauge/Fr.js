69aace911b6720ef566455faae0863c26a244c8e
TrucNguyen <truccm2001@gmail.com>
2023-01-06 22:11:48 +0700

    Update resouces

--- client/resources/langauge/Fr.js
+++ client/resources/langauge/Fr.js
@@ -9,11 +9,9 @@ export default function () {
     control_panel: 'Panneau de commande',
     temperature: 'Température',
     humidity: 'Humidité',
-    gas: 'Gas',
     lamp: 'Lampe',
     fan: 'Ventilateur',
     curtain: 'Rideau',
-    buzzer: 'Avertisseur d\'explosion de gaz',
     max: 'Max',
     min: 'Min',
     average: 'Moyenne',
@@ -32,6 +30,15 @@ export default function () {
     abutton: 'Ajouter un compte',
     passwd: 'Mot de passe',
     cre: 'Ajouter',
-    back: 'Arrière'
+    back: 'Arrière',
+    speaker: 'Sirène',
+    gas: 'Concentration de gaz',
+    h1: '1 Heure',
+    h2: '2 Heures',
+    h6: '6 Heures',
+    h12: '12 Heures',
+    d1: '1 Jour',
+    w1: '1 Semaine',
+    m1: '1 Mois'
   }
 }
